2d7222dd5583a7846fc21c629d68277a3df21bf5
Corentin MERCIER <corentin.mercier@epita.fr>
2017-11-05 00:54:52 +0100

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Text2ducky
+## ascii text to ducky script converter
+
+Simply want to use the rubber ducky to paste some text?  
+Use text2ducky.py python script to do so.
+
+### How to use it
+
+Copy the **text2ducky.py** file to your computer and type the following command:  
+```
+python3 text2ducky.py [input_file] 
+```
+
+The ducky script will be saved as inject.txt
---
+++ text2ducky.py
@@ -0,0 +1,33 @@
+################
+#              # 
+#  Text2ducky  #
+#              #
+################
+
+import sys
+import os
+
+# main function
+def main(input_file):
+    if not os.path.isfile(input_file):
+        # checks if the file exists
+        print(input_file + " : file not found")
+        return False
+    # clears the previous inject.txt file if it exists
+    if os.path.isfile("inject.txt"):
+        os.system("rm inject.txt")
+    
+    f = open("inject.txt", "w")
+    f.write("REM k4li \n")
+    # opens the input file
+    input_file_open = open(input_file, "r")
+    for line in input_file_open:
+        f.write("STRING " + line)
+    return True
+
+# the program
+if len(sys.argv) < 2:
+    print("you need to specify an input file")
+else:
+    main(sys.argv[1])
+
